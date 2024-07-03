a8440cc63b2a5020720a3f459fe2502d396ee07a
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-07-03 22:41:57 +0700

    :zap: Try do thread

---
+++ week-03/thread_lab.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 100
+
+struct {
+	int* n1;
+	int* n2;
+} data;
+
+void input_data(int i)
+{
+	printf("\033[0;36m\nProcess A: \n\033[0m");
+	printf("\033[0;36mNO_idx.[%d] :Enter Data First: \033[0m", i);
+	scanf("%d", &data.n1[i]);
+	printf("\033[0;36mNO_idx.[%d] :Enter Data Second: \033[0m", i);
+	scanf("%d", &data.n2[i]);
+}
+
+void* process_B(void* arg)
+{
+	int index = *(int*)arg;
+	sleep(3);
+	printf("\033[0;32mProcess B: NO.[%d] First data: %d \033[0m\n", index, data.n1[index]);
+	pthread_exit(NULL);
+}
+
+void* process_C(void* arg)
+{
+	int index = *(int*)arg;
+	if (index != 0)
+		sleep(3);
+	printf("\033[0;32mProcess C: NO.[%d] Second data: %d \033[0m\n", index, data.n2[index]);
+	pthread_exit(NULL);
+}
+
+int main(void)
+{
+	pthread_t threads[BUFFER_SIZE * 2];
+	int indices[BUFFER_SIZE];
+	int i = 0;
+
+	data.n1 = (int*)malloc(BUFFER_SIZE * sizeof(int));
+	data.n2 = (int*)malloc(BUFFER_SIZE * sizeof(int));
+
+	while (1)
+	{
+		input_data(i);
+		if (data.n1[i] < 0)
+		{
+			printf("Terminating program.\n");
+			break;
+		}
+		indices[i] = i;
+		pthread_create(&threads[i * 2], NULL, process_B, &indices[i]);
+		pthread_create(&threads[i * 2 + 1], NULL, process_C, &indices[i]);
+		i++;
+	}
+
+	for (int j = 0; j < i * 2; j++)
+		pthread_join(threads[j], NULL);
+
+	free(data.n1);
+	free(data.n2);
+	return 0;
+}
